c24368ed961da85ef26d66f8491fd3c1510d0efe
rafayKhandev <dandelpan@gmail.com>
2025-12-24 14:50:46 +0500

    change-embeddings

--- app/services/vectorstore.py
+++ app/services/vectorstore.py
@@ -6,10 +6,10 @@ from typing import List
 
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-# from langchain_community.embeddings import HuggingFaceEmbeddings
-# from langchain_community.vectorstores import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+# from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_chroma import Chroma
 
 
 DEFAULT_PERSIST_DIR = "data/chroma"
